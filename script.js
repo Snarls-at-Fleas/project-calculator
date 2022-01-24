93b8672223f8cbcd4f2aa91be84b22617409b2ff
Snarls-at-Fleas <berlin.pavel@gmail.com>
2022-01-24 07:50:39 +0300

    Change script to correctly display typed numbers, add basic math operation. Still not fully functional

--- script.js
+++ script.js
@@ -17,38 +17,63 @@ function divide(a, b){
 function operate(operator, a, b){
     let result = 0;
     switch (operator) {
-        case "add":
+        case "buttonAdd":
             result = add(a, b);
             break;
-        case "subtract":
+        case "buttonSubtract":
             result = subtract(a, b);
             break;
-        case "multiply":
+        case "buttonMultiply":
             result = multiply(a, b);
             break;
-        case "divide":
+        case "buttonDivide":
             result = divide(a, b);
     }
     return result;
 };
 
 function evalNumber(numberClicked) {
-    console.log(`Last digit(${numberClicked}) is added to typeBuffer. Display refreshed`);
     typeBuffer += numberClicked;
     display.textContent = typeBuffer;
+    console.log(`Last digit(${numberClicked}) is added to typeBuffer. Display refreshed`);    
+    console.log(typeBuffer, typeBuffer.length);
     // Might need separate variable to store real number, not text
     // Currently dec point doesn't work
+    // Also need to limit number of digits so they stay within the display
 };
 
 function evalMath(action) {
+    if (calcBuffer.length == 0) {
+        numberEntered = parseInt (typeBuffer, 10);
+        calcBuffer.push(numberEntered);
+        mathBuffer = action;
+        typeBuffer = "0";
+        display.textContent = typeBuffer;
+    } else {
+        numberEntered = parseInt (typeBuffer, 10);
+        calcBuffer.push(numberEntered);
+        mathBuffer = action;
+        tempResult = operate(mathBuffer, calcBuffer[0], calcBuffer[1]);    
+        typeBuffer = tempResult.toString();    
+        display.textContent = typeBuffer;
+    }
+    console.log(calcBuffer);
+    console.log(mathBuffer);
     console.log(`Math action of ${action} is performed. Buffers reset`)
 };
+// After the Math action is performed the first element of calcBuffer should be removed
+// IMPORTANT - calculator performs last action pressed so for example
+// '3' '+' '2' '-' gives '1', not '5' as it subtracts not adds
+// Also after Math action typing numbers doesn't work as intended
 
 function evalClear () {
+    typeBuffer = "0";
+    display.textContent = typeBuffer;
+    console.log(typeBuffer);
     console.log('All buffers reset. Display reset')
 };
 
-let typeBuffer = ""; // Stores numbers typed till the math button click (Make it an array?)
+let typeBuffer = "0"; // Stores numbers typed till the math button click (Make it an array?)
 let calcBuffer = []; // Stores last two numbers entered. They go here after math button click
 let mathBuffer = ""; // Stores last math action after math button click
 let buttons = document.querySelector(".buttons");
@@ -66,7 +91,12 @@ buttons.addEventListener('click', (e) => {
         case "buttonNine":
         case "buttonZero":
         case "buttonDecPoint":
-            evalNumber(e.target.textContent);
+            if (typeBuffer == "0" || calcBuffer.length > 0) {
+                typeBuffer = "";
+            }
+            if (typeBuffer.length < 9) {
+                evalNumber(e.target.textContent);
+            };
             break;
         case "buttonAdd":
         case "buttonSub":
